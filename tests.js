b41feb5fb9ee8b6a64923b426e5440fac80a38ef
Fernando Correa <fernandocorrea@gmail.com>
2012-10-25 09:21:05 -0200

    New test

--- tests.js
+++ tests.js
@@ -9,4 +9,6 @@ test( "Class Template", function() {
   ok( tmpl3.render({ble: "bli"}) == "bla ", "Template render empty templates" );
   var tmpl4 = new Template("bla <%= this.ble %>");
   ok( tmpl4.render({ble: "bli"}) == "bla bli", "Template render empty templates" );
+  var tmpl5 = new Template("<% for(var i = 0; i < this.array.length; i++){ %><%= this.array[i] %> <%= this.ble + i %> <% } %>");
+  ok( tmpl5.render({ble: "blo", array: ["one", "two"]}) == "one blo0 two blo1 ", "Template render empty templates" );
 });
