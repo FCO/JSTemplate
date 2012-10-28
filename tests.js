7c9d8fcde733c979f14f0159219ecd1471a6a6c7
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2012-10-28 19:50:59 -0200

    Spliting tests

--- tests.js
+++ tests.js
@@ -3,6 +3,8 @@ test( "Class Template", function() {
 	var tmpl = new Template("");
 	ok( tmpl != null, "Class template can create objects" );
 	equal( tmpl.render({}), "", "Template render empty templates" );
+});
+test( "Template", function() {
 	var tmpl2 = new Template("bla");
 	equal( tmpl2.render({}), "bla", "Template render not empty templates" );
 	var tmpl3 = new Template("bla <% data.ble %>");
@@ -11,6 +13,8 @@ test( "Class Template", function() {
 	equal( tmpl4.render({ble: "bli"}), "bla bli", "Template render not empty templates with code blocks with an '='" );
 	var tmpl5 = new Template("<% for(var i = 0; i < data.array.length; i++){ %><%= data.array[i] %> <%= data.ble + i %> <% } %>");
 	equal( tmpl5.render({ble: "blo", array: ["one", "two"]}), "one blo0 two blo1 ", "Template render not empty templates with code blocks with a complex js code" );
+});
+test( "Template Helpers", function() {
 	var tmpl6 = new Template("<%= this.helpers.test() %>");
 	tmpl6.addHelper("test", function(){ok(true, "Helper running inside the template"); return "my test"});
 	equal( tmpl6.render({}), "my test", "Using the return of the worker" );
