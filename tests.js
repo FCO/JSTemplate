d84345b3c52e8128c107336dc56bb1407b170625
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2012-10-28 19:52:29 -0200

    Renaming test vars

--- tests.js
+++ tests.js
@@ -5,20 +5,20 @@ test( "Class Template", function() {
 	equal( tmpl.render({}), "", "Template render empty templates" );
 });
 test( "Template", function() {
-	var tmpl2 = new Template("bla");
-	equal( tmpl2.render({}), "bla", "Template render not empty templates" );
-	var tmpl3 = new Template("bla <% data.ble %>");
-	equal( tmpl3.render({ble: "bli"}), "bla ", "Template render not empty templates with code blocks" );
-	var tmpl4 = new Template("bla <%= data.ble %>");
-	equal( tmpl4.render({ble: "bli"}), "bla bli", "Template render not empty templates with code blocks with an '='" );
-	var tmpl5 = new Template("<% for(var i = 0; i < data.array.length; i++){ %><%= data.array[i] %> <%= data.ble + i %> <% } %>");
-	equal( tmpl5.render({ble: "blo", array: ["one", "two"]}), "one blo0 two blo1 ", "Template render not empty templates with code blocks with a complex js code" );
+	var tmpl1 = new Template("bla");
+	equal( tmpl1.render({}), "bla", "Template render not empty templates" );
+	var tmpl2 = new Template("bla <% data.ble %>");
+	equal( tmpl2.render({ble: "bli"}), "bla ", "Template render not empty templates with code blocks" );
+	var tmpl3 = new Template("bla <%= data.ble %>");
+	equal( tmpl3.render({ble: "bli"}), "bla bli", "Template render not empty templates with code blocks with an '='" );
+	var tmpl4 = new Template("<% for(var i = 0; i < data.array.length; i++){ %><%= data.array[i] %> <%= data.ble + i %> <% } %>");
+	equal( tmpl4.render({ble: "blo", array: ["one", "two"]}), "one blo0 two blo1 ", "Template render not empty templates with code blocks with a complex js code" );
 });
 test( "Template Helpers", function() {
-	var tmpl6 = new Template("<%= this.helpers.test() %>");
-	tmpl6.addHelper("test", function(){ok(true, "Helper running inside the template"); return "my test"});
-	equal( tmpl6.render({}), "my test", "Using the return of the worker" );
-	var tmpl7 = new Template("str: <%= this.helpers.test('test string') %>");
-	tmpl7.addHelper("test", function(str){equal(str, "test string", "Helper running inside the template and receiving parameters"); return str});
-	equal( tmpl7.render({}), "str: test string", "Using the return of the worker" );
+	var tmpl1 = new Template("<%= this.helpers.test() %>");
+	tmpl1.addHelper("test", function(){ok(true, "Helper running inside the template"); return "my test"});
+	equal( tmpl1.render({}), "my test", "Using the return of the worker" );
+	var tmpl2 = new Template("str: <%= this.helpers.test('test string') %>");
+	tmpl2.addHelper("test", function(str){equal(str, "test string", "Helper running inside the template and receiving parameters"); return str});
+	equal( tmpl2.render({}), "str: test string", "Using the return of the worker" );
 });
